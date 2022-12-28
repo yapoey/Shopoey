91d94b13f1dbc8c372443a26508ca1f09522a1ef
yapoey <mohamed.medhat92@gmail.com>
2022-12-28 15:51:31 +0900

    seed project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
